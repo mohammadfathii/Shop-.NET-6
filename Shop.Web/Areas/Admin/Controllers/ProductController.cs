e61197ffdb072d4dcb0f4cfb8b24be1d30060add
Mohammad <m_fathi65@yahoo.com>
2023-06-22 12:17:35 +0330

    writing product part of admin

---
+++ Shop.Web/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Web.Data;
+
+namespace Shop.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        public ShopDBContext Context { get; set; }
+        public ProductController(ShopDBContext context)
+        {
+            Context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = Context.Products.Include(p => p.Discount).Include(p => p.Category).ToList();
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create() {
+            return View();
+        }
+
+    }
+}
